e7a762263a4a6ec76f4fa4bae2a98b6855cb0a21
James Langford <derivative7117@live.com>
2017-06-19 16:27:37 -0600

    simple multiplication

---
+++ Simple multiplication.js
@@ -0,0 +1,7 @@
+let simpleMultiplication = n => {
+    if(n % 2 === 0){
+        return 8 * n;
+    }else{
+        return 9 * n;
+    }
+}
---
+++ String Cleaning.js
@@ -0,0 +1,3 @@
+let stringClean = s => {
+    return s.replace(/[0-9]/g, '');
+}
